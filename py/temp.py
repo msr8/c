7af02604adca1f12849f6cc65fabe00c3a972682
msr8 <live.shashwat@gmail.com>
2024-02-21 17:59:40 +0530

    outside

--- py/temp.py
+++ py/temp.py
@@ -1,8 +1,20 @@
-def fun(n) :
-    k = 0
-    for i in range(n // 2, n):
-        j = 2
-        while j <= n:
-            k +=n// 2
-            i *= 2
-    return k
+data = {
+    'age':            [10,22,13,21,12,11,17],
+    'section':        ['A','B','C','B','B','A','A'],
+    'city':           ['Gurgaon','Delhi','Mumbai','Delhi','Mumbai','Delhi','Mumbai'],
+    'gender':         ['M','F','F','M','M','M','F'],
+    'favorite_color': ['red','black','yellow','pink','black','green','red']   
+}
+
+data_list = [
+    ['age', 'section', 'city',    'gender', 'favorite_color'],
+    [10,    'A',       'Gurgaon', 'M',      'red'           ],
+    [22,    'B',       'Delhi',   'F',      'black'         ],
+    [13,    'C',       'Mumbai',  'F',      'yellow'        ],
+    [21,    'B',       'Delhi',   'M',      'pink'          ],
+    [12,    'B',       'Mumbai',  'M',      'black'         ],
+    [11,    'A',       'Delhi',   'M',      'green'         ],
+    [17,    'A',       'Mumbai',  'F',      'red'           ]
+]
+
+df = pd.DataFrame(data_list[1:], columns=data_list[0])
